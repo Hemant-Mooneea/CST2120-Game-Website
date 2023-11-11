4c4abe878c525d06be1d6156b80e6bdc07010f5a
Hemant <hemantmooneea1@gmail.com>
2023-11-11 14:15:30 +0400

    Fixed issue with redirections on signup

--- login/scripts/login_signup.js
+++ login/scripts/login_signup.js
@@ -1,19 +1,21 @@
 function signupValidation() 
 {
     // Get the input data from the fields
-    const name = document.getElementById("name").value;
+    const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const gender = document.getElementById("gender").value;
     const date = document.getElementById("date").value;
 
-
-    if (name === "" || date === "" || gender === "" || password === "") {
+    if (username === "" || date === "" || gender === "" || password === "") 
+    {
         alert("Please fill in all fields");
         return false; // Do not proceed with form submission
     }
 
+    // Perform other validation or actions if needed
     window.location.href = "../homepage/homepage.html";
 }
+
 function nameValidation()
 {
 
