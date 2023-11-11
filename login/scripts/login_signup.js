d988dfa615ee1012655034125c1f96e265aad546
Hemant <hemantmooneea1@gmail.com>
2023-11-11 17:16:10 +0400

    Partially done with username logic checks

--- login/scripts/login_signup.js
+++ login/scripts/login_signup.js
@@ -1,3 +1,9 @@
+/* 
+This function is called when the user presses the signup button.
+It initialises the constants by first obtaining it from the the form
+It then calls each of the validation functions to check for errors
+*/
+const special_regex = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/;
 function signupValidation() 
 {
     // Get the input data from the fields
@@ -5,8 +11,8 @@ function signupValidation()
     const password = document.getElementById("password").value;
     const gender = document.getElementById("gender").value;
     const date = document.getElementById("date").value;
-
-    if (username === "" || date === "" || gender === "" || password === "") 
+    usernameValidation(username);
+    if (date === "" || gender === "" || password === "") 
     {
         alert("Please fill in all fields");
         return false; // Do not proceed with form submission
@@ -16,7 +22,38 @@ function signupValidation()
     window.location.href = "../homepage/homepage.html";
 }
 
-function nameValidation()
+/* 
+This Function takes in the username and checks whether:
+    the username already exists
+    the username contains special characters
+    the user did not input a username
+*/
+function usernameValidation(username)
+{   
+    
+    //Don't forget to check json files if name already exists
+    //Checks if username is empty
+    if (username === "")
+    {
+        alert("Username cannot be empty");
+        return false;
+    
+    }
+    //Checks if username has special characters
+    if (special_regex.test(username))
+    {
+        alert("Username cannot contains special characters");
+        return false;
+    }
+    //Checks if username is longer than 20 characters
+    if (username.length > 20)
+    {
+        alert("Username cannot be longer than 20 characters");
+        return false;
+    }
+    
+}
+function passwordValidation(password)
 {
 
 }
