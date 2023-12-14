f813323d60fda79e0bc98574ff35ac0e60da4c53
Hemant <hemantmooneea1@gmail.com>
2023-12-14 10:19:43 +0400

    You need to login to play now

--- gamepage/scripts/game.js
+++ gamepage/scripts/game.js
@@ -10,6 +10,23 @@ window.onload = function()
     // Get the height and width of the div
     const SCREEN_HEIGHT = divElement.clientHeight;
     const SCREEN_WIDTH = divElement.clientWidth;
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+    if (!isLoggedIn) 
+    {
+         // Create a message to prompt the user to log in
+        const loginPrompt = document.createElement('p');
+        loginPrompt.textContent = 'Log in to play the game';
+        loginPrompt.style.color = 'white';
+        loginPrompt.style.fontSize = '50px';
+        loginPrompt.style.textAlign = 'center';
+        loginPrompt.style.position = 'absolute';
+        loginPrompt.style.top = '45%';
+        loginPrompt.style.transform = 'translateY(-50%)'; 
+        loginPrompt.style.width = '100%';
+        divElement.appendChild(loginPrompt);
+        }
+    else
+    {
     new Phaser.Game({
         width: SCREEN_WIDTH, // Width of the game in pixels
         height: SCREEN_HEIGHT, // Height of the game in pixels
@@ -18,4 +35,5 @@ window.onload = function()
         physics: { default: 'arcade' }, // The physics engine to use
         parent: 'game', // Create the game inside the <div id="game"> 
     });
+}
 }   
